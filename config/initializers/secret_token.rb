c92a3cf50c5638d13a4f5dc34096a4e9ab1c0ba2
MCajayon <marvi.cajayon@gmail.com>
2014-01-16 14:25:15 +0900

    Initialize Repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Transmail::Application.config.secret_key_base = '2bb92ef537a920e393378c8c3569876273a63c869d9723c5f0236bfc14665c25a2c5a188eb2e8f83fc526b0ee6f272d44a704105b19b771bf24438fe4bdac164'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Transmail::Application.config.session_store :cookie_store, key: '_transmail_session'
